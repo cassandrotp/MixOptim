349d66a3dd872356916ed9805801d5954ddd476f
cassandrotp <45265186+cassandrotp@users.noreply.github.com>
2020-06-28 23:38:25 -0300

    Add files via upload

--- R/mixtureRangeOptim.R
+++ R/mixtureRangeOptim.R
@@ -6,14 +6,15 @@
 #'
 #' @param functions An array of functions
 #' @param desirabilityModel A desirability overallD model
-#' @param startPoint An array with the references (mid-points) for the optimization
-#' @param step The ammount of each increment in the optimization
+#' @param midPoints An array with the references (mid-points) for the optimization
 #' @param alpha Defines the range of the seach, as startPoint +- alpha for each x value
+#' @param step The ammount of each increment in the optimization
+#' @param plot Define is the data frame that can be used for the desirabilityPlot function will be create. Strongly affects performance
 #' @return A list containg the data regarding the maximum desirability found
 #' @export
-mixtureRangeOptim <- function(funcoes, modeloDesej, midPoints, alpha, passo = 0.01, plot = T) {
+mixtureRangeOptim <- function(functions, desirabilityModel, midPoints, alpha, step = 0.01, plot = T) {
 
-    misturaRecursao <- function(funcoes, modeloDesej, passo, level, value, plot) {
+    misturaRecursao <- function(functions, desirabilityModel, step, level, value, plot) {
     value[level] = midPoints[level] + alpha[level]
     if(level == length(value)) {
       #print(value)
@@ -22,11 +23,11 @@ mixtureRangeOptim <- function(funcoes, modeloDesej, midPoints, alpha, passo = 0.
       if(gap >= midPoints[level] - alpha[level]) {
         value[level] = gap
         #print(value)
-        valores = rep(0.0, length(funcoes))
-        for(i in 1:length(funcoes)) {
-          valores[i] = funcoes[[i]](value)
+        valores = rep(0.0, length(functions))
+        for(i in 1:length(functions)) {
+          valores[i] = functions[[i]](value)
         }
-        tmp <- predict(modeloDesej, as.data.frame(matrix(valores, nrow=1)))
+        tmp <- predict(desirabilityModel, as.data.frame(matrix(valores, nrow=1)))
         if(plot)
           grafico <<- rbind(grafico, c(value, tmp, valores))
         if(tmp > best) {
@@ -36,8 +37,8 @@ mixtureRangeOptim <- function(funcoes, modeloDesej, midPoints, alpha, passo = 0.
       }
     } else {
       while(value[level] >= midPoints[level] - alpha[level]) {
-        misturaRecursao(funcoes, modeloDesej, passo, level + 1, value, plot)
-        value[level] = value[level] - passo
+        misturaRecursao(functions, desirabilityModel, step, level + 1, value, plot)
+        value[level] = value[level] - step
       }
     }
 
@@ -53,10 +54,10 @@ mixtureRangeOptim <- function(funcoes, modeloDesej, midPoints, alpha, passo = 0.
     vetor <- midPoints - alpha
     #print(vetor)
     count <- 0
-    steps = (alpha[1]*2)/(passo)
+    steps = (alpha[1]*2)/(step)
     while(vetor[1] <= midPoints[1] + alpha[1]) {
-      misturaRecursao(funcoes, modeloDesej, passo, 2, vetor, plot)
-      vetor[1] = vetor[1] + passo
+      misturaRecursao(functions, desirabilityModel, step, 2, vetor, plot)
+      vetor[1] = vetor[1] + step
       count = count + 1
       perc = (count * 100.0) / steps
       if(perc %% 10 == 0) {
